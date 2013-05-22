a98224611ac1986ec4dd7e7398d49711564574eb
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2013-05-23 00:50:33 +0530

    added primitive sockets

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 var board = require('./board.json');
 
-game(board); //starts the game
+var http = require('http');
+var socketio = require('socket.io');
+var fs = require('fs');
+
+var app = http.createServer(handler);
+var io = socketio.listen(app);
+app.listen(8000);
+
+function handler(req, res)
+{
+	fs.readFile(__dirname+'/index.html',function(err, data){
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
+}
+
+io.sockets.on('connection',function(socket){
+	console.log('socket created\n');
+	var cP = 1;
+	var boardObj = board;
+	printBoard(boardObj);
+	socket.on('action',function(data){
+		cP = action(boardObj,cP,data.substr(0,1),data.substr(1,1));
+		socket.emit('result',{cell:data,cP:cP*(-1)});
+		var boardStatus = checkWin(boardObj);
+		if(boardStatus != null)
+		socket.emit('win',boardStatus);
+	});
+});
+
+
+//game(board); //starts the game
 
 function game(boardObj)
 {
@@ -8,12 +43,12 @@ function game(boardObj)
 	
 	printBoard(boardObj);
 	//input should be taken here, sample input given below
-	currentPlayer = (-1) * action(boardObj,currentPlayer,2,2);
-	currentPlayer = (-1) * action(boardObj,currentPlayer,2,3);
-	currentPlayer = (-1) * action(boardObj,currentPlayer,1,2);
-	currentPlayer = (-1) * action(boardObj,currentPlayer,3,3);
-	currentPlayer = (-1) * action(boardObj,currentPlayer,1,1);
-	currentPlayer = (-1) * action(boardObj,currentPlayer,1,3);
+	currentPlayer = action(boardObj,currentPlayer,2,2);
+	currentPlayer = action(boardObj,currentPlayer,2,3);
+	currentPlayer = action(boardObj,currentPlayer,1,2);
+	currentPlayer = action(boardObj,currentPlayer,3,3);
+	currentPlayer = action(boardObj,currentPlayer,1,1);
+	currentPlayer = action(boardObj,currentPlayer,1,3);
 	//updateBoard(boardObj,currentPlayer,i,j);
 	//then change currentPlayer to opposite;
 }
@@ -25,16 +60,16 @@ function action(boardObj,currentPlayer,i,j)
 	else
 	{
 		console.log('invalid option');
-		return currentPlayer*(-1);
+		return currentPlayer;
 	}
 	printBoard(boardObj);
 	checkWin(boardObj);
-	return currentPlayer;
+	return currentPlayer*(-1);
 }
 
 function printBoard(boardObj)
 {
-	for(var i=0;i<=4;i++)
+	for(var i=0;i<4;i++)
 	{
 		var toPrint = '';
 		for(var j=0;j<=4;j++)
@@ -50,19 +85,20 @@ function printBoard(boardObj)
 
 function checkWin(boardObj)
 {
-	for(var i=0;i<=4;i++)
+	for(var i=0;i<4;i++)
 	{
-		if(boardObj[0][i] == 3 || boardObj[i][0]==3)
+		if(boardObj[0][i] == 3 || boardObj[i][0]==3 || boardObj[0][4] == 3)
 		{
 			console.log('Positive Wins!');
-			return;
+			return 'Positive Wins!';
 		}
-		else if(boardObj[0][i] == -3 || boardObj[i][0]==-3)
+		else if(boardObj[0][i] == -3 || boardObj[i][0]==-3 || boardObj[0][4] == -3)
 		{
 			console.log('Negative Wins!');
-			return;
+			return 'Negative Wins!';
 		}
 	}
+	return '';
 }
 
 function updateBoard(boardObj,inNum,i,j)
